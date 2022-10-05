d3cd821524f10fdd9ef432b399089aaec8159ebc
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-06 00:37:38 +0800

    Fix default values for playerNames

--- main.js
+++ main.js
@@ -10,29 +10,19 @@ const players = (() => {
 
   const players = [];
   
-  function getPlayers() {
+  function _getPlayers() {
 
-    let player1Name = document.querySelector('#player1').value;
-    let player2Name = document.querySelector('#player2').value;
+    let player1Name = document.querySelector('#player1');
+    let player2Name = document.querySelector('#player2');
     
-    if(!player1Name) {
-      player1Name = 'Player 1';
-    } else if(!player2Name) {
-      player2Name = 'Player 2';
-    } else {
-      player1Name.value;
-      player2Name.value;
-    }
-
-    const player1 = new Players(player1Name, 'X');
-    const player2 = new Players(player2Name, 'O');
+    const player1 = new Players(player1Name.value, 'X');
+    const player2 = new Players(player2Name.value, 'O');
 
     players.push(player1);
     players.push(player2);
-
   }
 
-  function toggle () {
+  function _toggle () {
     const form = document.querySelector('#form');
     const board = document.querySelector('.container');
 
@@ -41,13 +31,14 @@ const players = (() => {
   }
 
   return {
-    getPlayers,
-    toggle,
+    _getPlayers,
+    _toggle,
+    displayPlayers,
   }
 })();
 
 const startBtn = document.querySelector('#startBtn');
 startBtn.addEventListener('click', () => {
-  players.getPlayers();
-  players.toggle();
+  players._getPlayers();
+  players._toggle();
 });
