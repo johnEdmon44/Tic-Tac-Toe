288d354897d18a3ef986ddbcf037bcbad2cdf9be
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-05 22:09:44 +0800

    Toggle board visibility on startgame btn

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+//get and display players
+const players = (() => {
+
+  function toggle () {
+    const form = document.querySelector('#form');
+    const board = document.querySelector('.container');
+
+    form.classList.toggle('visible');
+    board.classList.toggle('visible');
+  }
+
+  return {
+    toggle,
+  }
+})();
+
+const startBtn = document.querySelector('#startBtn');
+startBtn.addEventListener('click', () => {
+
+  players.toggle();
+});
