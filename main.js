20004282397281d05d77eacd7a33d4bae63665ca
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-05 22:14:19 +0800

    Create object for playername and weapons

--- main.js
+++ main.js
@@ -3,6 +3,24 @@
 //get and display players
 const players = (() => {
 
+  function Players (name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+
+  const players = [];
+  
+  function getPlayers() {
+    const player1Name = document.querySelector('#player1');
+    const player2Name = document.querySelector('#player2'); 
+    const player1 = new Players(player1Name.value = 'player1', 'X');
+    const player2 = new Players(player2Name.value = 'player2', 'O');
+
+    players.push(player1);
+    players.push(player2);
+
+  }
+
   function toggle () {
     const form = document.querySelector('#form');
     const board = document.querySelector('.container');
@@ -12,12 +30,13 @@ const players = (() => {
   }
 
   return {
+    getPlayers,
     toggle,
   }
 })();
 
 const startBtn = document.querySelector('#startBtn');
 startBtn.addEventListener('click', () => {
-
+  players.getPlayers();
   players.toggle();
 });
