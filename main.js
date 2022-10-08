3c18e928319cc81ec12c083b9397e9f32c8cb71c
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-08 16:33:26 +0800

    Add a condition to get the winner

--- main.js
+++ main.js
@@ -63,8 +63,102 @@ const game = (() => {
     });
   }
 
+  function checkWinner () {
+    const goal0 = document.querySelector('[data-index = "0"]').textContent;
+    const goal1 = document.querySelector('[data-index = "1"]').textContent;
+    const goal2 = document.querySelector('[data-index = "2"]').textContent;
+    const goal3 = document.querySelector('[data-index = "3"]').textContent;
+    const goal4 = document.querySelector('[data-index = "4"]').textContent;
+    const goal5 = document.querySelector('[data-index = "5"]').textContent;
+    const goal6 = document.querySelector('[data-index = "6"]').textContent;
+    const goal7 = document.querySelector('[data-index = "7"]').textContent;
+    const goal8 = document.querySelector('[data-index = "8"]').textContent;
+
+    if (
+      goal0 === 'X' &&
+      goal1 === 'X' &&
+      goal2 === 'X' ||
+
+      goal0 === 'X' &&
+      goal3 === 'X' &&
+      goal6 === 'X' ||
+
+      goal1 === 'X' &&
+      goal4 === 'X' &&
+      goal7 === 'X' ||
+
+      goal3 === 'X' &&
+      goal4 === 'X' &&
+      goal5 === 'X' ||
+
+      goal6 === 'X' &&
+      goal7 === 'X' &&
+      goal8 === 'X' ||
+
+      goal2 === 'X' &&
+      goal5 === 'X' &&
+      goal8 === 'X' ||
+
+      goal0 === 'X' &&
+      goal4 === 'X' &&
+      goal8 === 'X' ||
+
+      goal2 === 'X' &&
+      goal4 === 'X' &&
+      goal6 === 'X'
+    ) {
+      playerX = true;
+      console.log('X')
+      return playerX
+    } else if (
+      goal0 === 'O' &&
+      goal1 === 'O' &&
+      goal2 === 'O' ||
+
+      goal0 === 'O' &&
+      goal3 === 'O' &&
+      goal6 === 'O' ||
+
+      goal1 === 'O' &&
+      goal4 === 'O' &&
+      goal7 === 'O' ||
+
+      goal3 === 'O' &&
+      goal4 === 'O' &&
+      goal5 === 'O' ||
+
+      goal6 === 'O' &&
+      goal7 === 'O' &&
+      goal8 === 'O' ||
+
+      goal2 === 'O' &&
+      goal5 === 'O' &&
+      goal8 === 'O' ||
+
+      goal0 === 'O' &&
+      goal4 === 'O' &&
+      goal8 === 'O' ||
+
+      goal2 === 'O' &&
+      goal4 === 'O' &&
+      goal6 === 'O'
+    ) {
+      playerO = true;
+      console.log('O')
+      return playerO
+    }
+
+    if(data.length === 9 && !playerO && !playerX) {
+      draw = true;
+      console.log('draw')
+      return draw;
+    }
+  }
+
+
   return {
     addDataAtt,
+    checkWinner,
   }
 
 })(); 
@@ -73,6 +167,9 @@ const data = []; // data attribute use to check the winners
 const weapons = []; // to alternate between X and O
 
 //
+let playerX;
+let playerO;
+let draw;
 const boxes = document.querySelectorAll('.items');
 boxes.forEach( x => {
   x.addEventListener('click', e => {
@@ -90,5 +187,7 @@ boxes.forEach( x => {
       weapons.push('O');
       e.target.textContent = 'O';
     }
+
+    game.checkWinner();
   });
 });
